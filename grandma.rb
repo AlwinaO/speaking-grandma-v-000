442da88759f505483de6ed3ab22bc79ea1897b3f
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-11-11 02:32:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -8,6 +8,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   end
 end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
@@ -19,3 +20,4 @@ end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
