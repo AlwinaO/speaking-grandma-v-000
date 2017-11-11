5ae6bf28f10539ad8c850e55209c1ac548e56db9
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2017-11-11 01:58:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase()
     return "I LOVE YOU TOO PUMPKIN!"
-  else phrase.upcase() == phrase.upcase()
+  else phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
   end
 end
